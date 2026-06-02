a09cb293c7a4a804c63ba3a7fd696e140c7e1a19
Gabriel Maués <g1brielmaues@gmail.com>
2026-06-02 11:16:23 -0400

    helpers

--- playwright/support/helpers.ts
+++ playwright/support/helpers.ts
@@ -12,9 +12,4 @@ export function generateOrderCode() {
     return `${prefix}-${randomPart}`;
 }
 
-import { Page } from '@playwright/test'
 
-export async function searchOrder(page: Page, orderNumber: string) {
-  await page.getByRole('textbox', { name: 'Código do Pedido' }).fill(orderNumber)
-  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
-}
